d299219e847bcb652dbc9a5e65b8ebbd88a5891d
chimny04 <lucia.roback@gmail.com>
2023-11-30 15:29:00 +0900

    13-2

--- main.c
+++ main.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-// 형식 선언 
-struct student {
-    int ID;          // 학번 
-    char name[100];  // 이름 
-    double score;    // 학점 
+struct point {
+    int x;
+    int y;
 };
 
 int main(int argc, char *argv[])
 {
-    // instance 선언 
-    struct student s1 = {123, "sohee", 4.3 };
+    struct point p1, p2;
+    int xdiff, ydiff;
+    double dist;
     
-    // 다른 값 변경 
-    s1.ID = 12345;
-    s1.name[0] = 'c';
-    s1.score = 0.7;
+    printf("input p1 coordinate (x y): ");
+    scanf("%d %d", &p1.x, &p1.y);
     
-    // 출력 
-    printf("ID: %i \nname: %s \nscore: %.2f\n", s1.ID, s1.name, s1.score);
+    printf("input p2 coordinate (x y): ");
+    scanf("%d %d", &p2.x, &p2.y);
+    
+    // distance
+    xdiff = p2.x - p1.x;
+    ydiff = p2.y - p1.y;
+    dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+    printf("distance : %f\n", dist);
     system("PAUSE");	
     return 0;
 }
